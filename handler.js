e7cffc0cefa5f075ae01da51ecfe34960d7b3c63
Hai Bui <hai.bui@saigontechnology.com>
2019-12-18 15:58:38 +0700

    add db

--- handler.js
+++ handler.js
@@ -1,18 +1,21 @@
-'use strict';
+const { GraphQLServerLambda } = require("graphql-yoga");
+var fs = require("fs")
 
-module.exports.hello = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+const typeDefs = fs.readFileSync("./schema.gql").toString('utf-8');
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+const resolvers = {
+    Query: {
+        mysql_getUser: require("./resolver/Query/mysql_getUser").func,
+    },
+    Mutation: {
+        mysql_createUser: require("./resolver/Mutation/mysql_createUser").func,
+    }
 };
+
+const lambda = new GraphQLServerLambda({
+    typeDefs,
+    resolvers
+});
+
+exports.server = lambda.graphqlHandler;
+exports.playground = lambda.playgroundHandler;
