f953fb748207969efd63b40c72416a8838d475e3
Hai Bui <buixuanhai.it@gmail.com>
2019-12-19 07:57:24 +0700

    add response lib

--- handler.js
+++ handler.js
@@ -1,19 +1,16 @@
-import {db} from './common/Db'
+import { db } from './common/Db'
+import { success, failure } from './common/response-lib'
+
 export const hello = async event => {
-  const  users = await db.query(`
+  const users = await db.query(`
   select id, uuid, name from users `)
-    return {
-      statusCode: 200,
-      body: JSON.stringify(
-        {
-          message: 'Go Serverless v1.0! Your function executed successfully!',
-          input: event,
-          users
-        },
-        null,
-        2
-      ),
-    };
+
+  return success({
+    message: 'Go Serverless v1.0! Your function executed successfully!',
+    input: event,
+    users
+  })
+
 };
 
 // exports.server = lambda.graphqlHandler;
